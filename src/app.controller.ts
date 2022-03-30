25b753c50a506ca7349504ba21a41ab9fcfb9c94
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2022-03-30 17:53:33 +0300

    s90

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
 // import axios from 'axios'
-import { AppService } from './app.service';
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getHello() {
-    // const {data} = await axios.get('https://superapp-boldinov-default-rtdb.firebaseio.com/Arr/users.json')
-    // return data
-  }
+   @Get()
+   async getHello() {
+      // const {data} = await axios.get('https://superapp-boldinov-default-rtdb.firebaseio.com/Arr/users.json')
+      // return data
+   }
 }
--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,2 +1,1 @@
-export class AuthDto {
-}
+export class AuthDto {}
