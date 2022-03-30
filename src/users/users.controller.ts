d23322ac4e331cc5b89f4bdb898905b946e31836
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2022-03-30 16:05:11 +0300

    s90

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,39 @@
 import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Body } from '@nestjs/common'
+import { Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
-import { UsersService } from './users.service'
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor( private readonly userService: UsersService) {
-    }
+   constructor(private readonly userService: UsersService) { }
 
-    @Get()
-    getAll(): Promise<User[]> {
-        return this.userService.getAll()
-    }
+   @Get()
+   getAll(): Promise<User[]> {
+      return this.userService.getAll();
+   }
 
-    @Get(':id')
-    getOne(@Param('id') id: string): Promise<User> {
-        return this.userService.getById(id)
-    }
+   @Get(':id')
+   getOne(@Param('id') id: string): Promise<User> {
+      return this.userService.getById(id);
+   }
 
-    @Post()
-    create(@Body() createUserDto: CreateUserDto): Promise<User> {
-        return this.userService.create(createUserDto)
-    }
+   @Post()
+   create(@Body() createUserDto: CreateUserDto): Promise<User> {
+      return this.userService.create(createUserDto);
+   }
 
-    @Delete(':id')
-    remove(@Param('id') id: string): Promise<User> {
-        return this.userService.remove(id)
-    }
+   @Delete(':id')
+   remove(@Param('id') id: string): Promise<User> {
+      return this.userService.remove(id);
+   }
 
-    @Put(':id')
-    update(@Body() updateUserDto: UpdateUserDto, @Param('id') id: string): Promise<User> {
-        return this.userService.update(id, updateUserDto)
-    }
+   @Put(':id')
+   update(
+      @Body() updateUserDto: UpdateUserDto,
+      @Param('id') id: string,
+   ): Promise<User> {
+      return this.userService.update(id, updateUserDto);
+   }
 }
