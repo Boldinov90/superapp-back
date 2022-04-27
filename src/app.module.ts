c2050235bc636923b0e5085a750c56d41e0b3dac
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2022-04-27 17:42:52 +0300

    s90

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-// import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
 import { TypegooseModule } from 'nestjs-typegoose'
 import { getMongoConfig } from './configs/mongo.config'
@@ -10,7 +9,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
    imports: [
-      // UsersModule,
+      // Работает только с VPN
+      // MongooseModule.forRoot(
+      //    'mongodb+srv://bolding:boldinov123@cluster0.voiku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      // ),
       MongooseModule.forRoot('mongodb://localhost:27017/superapp'),
       ConfigModule.forRoot(),
       TypegooseModule.forRootAsync({
